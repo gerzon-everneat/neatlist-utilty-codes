aff36b77506cef5184d90a2bbf53636c5f23dea2
Everneat <gerzon@everneat.co>
2023-06-08 00:59:29 +0800

    update pageSetup and create Message Object

--- modified/UserAccount.js
+++ modified/UserAccount.js
@@ -1,39 +1,60 @@
-class UserAccount {
-
-    constructor() {
-        this.account_id = document.querySelector('[data-user_account_id]');
-        this.email = document.querySelector('[data-email]');
-        this.account_type = document.querySelector('[data-account-type]');
-        this.fullName = document.querySelector('[data-fullName]');
-        this.pageOwner = document.querySelector('[data-page-owner]');
-        this.sessioned = document.querySelector('[data-sessioned]');
-    }
-
-    
-    //generate the getter for the account_id, email, account_type, fullName, pageOwner, sessioned now
-    _account_id() {
-        return this.account_id;
-    } 
-
-    _email() {
-        return this.email;
-    }
-
-    _account_type() {
-        return this.account_type;
-    }
-
-    _fullName() {
-        return this.fullName;
-    }
-
-    _pageOwner() {
-        return this.pageOwner;
-    }
-
-
-
-    
-
-
+<script>
+class Message {
+	constructor(Owner, inbox) {
+  	this.sender = Owner._accountDetails().email
+    this.receiver = inbox.field_751728 == this.sender ? inbox.field_751729 : inbox.field_751728
+    this.message = null
+    this.chatID = inbox.field_751726[0].id
+  }
+  _sendMessage() {
+  const valid = document.querySelector('[data-desktop-message-text]').value.replace(/\n/g, "").replace(/\s/g, "").trim()
+  if(valid) {
+    this.message = document.querySelector('[data-desktop-message-text]').value.trim()
+  } else {
+    this._messageSentFaild()
+    return
+  }
+  const data =  {
+    "field_751726" : [this.chatID],
+    "field_751728" : this.sender,
+    "field_751729" : this.receiver,
+    "field_751731" : this.message
+  }
+      add(data, 118033, neatlistB.key)
 }
+
+  _createChatId() {
+    const data =  {
+      "field_751723" : this.sender,
+      "field_751724" : this.receiver
+    }
+    add(data, 118032, neatlistB.key)
+  }
+  _messageSentSuccess() {
+    this.__sendMessage().then(value=>{
+      const mineOrNot=""
+      const Name = "You"
+      const dayTime = "Now"
+      $(".messages-grid").append(`
+           <div id="w-node-_8dcf25fe-814e-2de8-0469-39cf8fd09a67-06e84186" class="${mineOrNot}my-message">
+            <div class="message-header-convo w-row">
+                <div class="w-col w-col-6">
+                    <h5 class="sender-receiver">${Name}</h5>
+                </div>
+                <div class="w-col w-col-6">
+                    <div class="date-time">${dayTime}</div>
+                </div>
+            </div>
+            <div class="message-header-convo message-box not-my-message-box">
+                <div class="${mineOrNot}my-message-text">${value.text}</div>
+            </div>
+         </div>
+      `)
+      })
+    }
+  _messageSentFaild() {
+  console.log("Sowething went Wrong!")
+  }
+}
+
+</script>
